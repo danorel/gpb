d073361fe0b2a7665430e38ad143a77aa7b924a9
danorel <mail.ordan@gmail.com>
2022-12-03 21:53:58 +0200

    Add Message/Server classes with Protobuf implementation

---
+++ pb-java/README.md
@@ -0,0 +1,6 @@
+### How to run
+
+Insert such command in terminal from the root of the package:
+```shell
+ protoc -I=src/main/java --java_out=src/main/java Message.proto
+```
---
+++ pb-java/src/main/java/Server.java
@@ -0,0 +1,24 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Server {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: output file is absent in command line call");
+            System.exit(-1);
+        }
+        Message.UserMessage message = Message.UserMessage.newBuilder()
+                .setId(1)
+                .setTitle("Greetings from a friend")
+                .setDescription("Hello, it's me! Glad to see you!")
+                .build();
+        FileOutputStream output;
+        try {
+            output = new FileOutputStream(args[0]);
+            message.writeTo(output);
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
